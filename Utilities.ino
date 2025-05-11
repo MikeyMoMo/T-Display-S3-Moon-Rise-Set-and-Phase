2a8ea73361e2e81628eb614efe6b0e4ba488f528
Mike Morrow <15792417+MikeyMoMo@users.noreply.github.com>
2025-05-12 05:31:57 +0800

    Fix error in menu item hold & exit

--- Utilities.ino
+++ Utilities.ino
@@ -13,6 +13,7 @@ void ShowFuturePhases()
     return (low <= high) ? (v >= low && v <= high) : (v >= low || v <= high);
   };
 
+  tft.setTextDatum(TL_DATUM);
   tft.fillScreen(TFT_BLACK);
   tft.fillRect(0, 0, tft.width(), dispLine5, RGB565(150, 53, 73));
   time_t epoch = get_epoch_time();
@@ -40,35 +41,35 @@ void ShowFuturePhases()
     if (inRange(moon.angle, 355, 5)) {
       if (!NM_Done) {
         NM_Done = true;
-        tft.drawString("New Moon ", 0, useLine, 4);
+        tft.drawString("New Moon", 4, useLine, 4);
         tft.drawString(chBuffer, 170, useLine, 4); useLine += font4Height;
       }
     }
     if (inRange(moon.angle, 85, 95)) {
       if (!FQ_Done) {
         FQ_Done = true;
-        tft.drawString("First Quarter ", 0, useLine, 4);
+        tft.drawString("First Quarter", 4, useLine, 4);
         tft.drawString(chBuffer, 170, useLine, 4); useLine += font4Height;
       }
     }
     if (inRange(moon.angle, 175, 185)) {
       if (!FM_Done) {
         FM_Done = true;
-        tft.drawString("Full Moon ", 0, useLine, 4);
+        tft.drawString("Full Moon", 4, useLine, 4);
         tft.drawString(chBuffer, 170, useLine, 4); useLine += font4Height;
       }
     }
     if (inRange(moon.angle, 265, 285)) {
       if (!LQ_Done) {
         LQ_Done = true;
-        tft.drawString("Last Quarter ", 0, useLine, 4);
+        tft.drawString("Last Quarter", 4, useLine, 4);
         tft.drawString(chBuffer, 170, useLine, 4); useLine += font4Height;
       }
     }
     epoch += 3600;  // Check next hour.
   }
   tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-  tft.drawString("Press either button to return.", 0, dispLine6, 4);
+  tft.drawString("Press either button to return.", 1, dispLine6, 4);
 }
 /*******************************************************************************************/
 void CheckButtons()
@@ -108,7 +109,12 @@ void CheckButtons()
 
     ledcWrite(TFT_BL, tftBL_Lvl);
     BLChangeMillis = millis();
-    delay(50);
+    //    Serial.printf("Brightness %i\r\n", tftBL_Lvl);
+    tft.setTextColor(TFT_YELLOW, RGB565(0, 0, 166));
+    tft.setTextPadding(tft.width());
+    tft.drawString("Brightness " + String(tftBL_Lvl), 4, dispLine6 + 5, 4);
+    tft.setTextPadding(0);
+    delay(100);
   }
   while ((digitalRead(decrPin)) == 0 &&
          (tftBL_Lvl >= MIN_BRIGHTNESS))
@@ -131,7 +137,12 @@ void CheckButtons()
 
     ledcWrite(TFT_BL, tftBL_Lvl);
     BLChangeMillis = millis();
-    delay(50);
+    //    Serial.printf("Brightness %i\r\n", tftBL_Lvl);
+    tft.setTextColor(TFT_YELLOW, RGB565(0, 0, 166));
+    tft.setTextPadding(tft.width());
+    tft.drawString("Brightness " + String(tftBL_Lvl), 4, dispLine6 + 5, 4);
+    tft.setTextPadding(0);
+    delay(100);
   }
 }
 /*******************************************************************************************/
@@ -203,25 +214,25 @@ void doMenu()
       if (digitalRead(incrPin) == 0) {  // Still pressed?
         switch (highlight)
         {
-          case 1:  // case 'B':
+          case 1:
             ShowFuturePhases();
-            //            Serial.println(digitalRead(incrPin));
-            //            Serial.println(digitalRead(decrPin));
             while ((digitalRead(incrPin) == 1) && (digitalRead(decrPin) == 1));
-            return;
-            break;
+            return; break;
 
-          //          case 2:  // case 'F':
-          //            break;
+          // case 2:
+          //   while ((digitalRead(incrPin) == 0) || (digitalRead(decrPin) == 0));
+          //   break;
           //
-          //          case 3:  // case 'V':
-          //            break;
-          //
-          case 4:  // Return to clock
-            return;
-            //
-            //          default:
-            //            break;  // Do nothing if not found.
+          // case 3:
+          //   while ((digitalRead(incrPin) == 0) || (digitalRead(decrPin) == 0));
+          //   break;
+
+          case 4:
+            while ((digitalRead(incrPin) == 1) && (digitalRead(decrPin) == 1));
+            return; break;
+
+          default:
+            return; break;
         }
       }
       menuHide = millis() + MENU_HIDE_TIME;  // Extend auto close time.
@@ -250,8 +261,13 @@ void setHourBrightness()
 
   // Serial.printf("tftBL_Lvl now set to %i\r\n", tftBL_Lvl);
 
-  if (!WakeUp) tftBL_Lvl = 0;
-  if (tftBL_Lvl > 500) tftBL_Lvl = defaultBright;  // == 1000 is not working.  Why?
+  // If the user has not changed the value and it is sleep time, turn off the display.
+  if (!WakeUp) {  // If in sleep time...
+    if (tftBL_Lvl > MAX_BRIGHTNESS) tftBL_Lvl = 0;  // 1000 means leave display off
+  } else {
+    // if it IS WakeUp time and the default 1000 is still in force, set to default.
+    if (tftBL_Lvl > MAX_BRIGHTNESS) tftBL_Lvl = defaultBright; // == 1000 no worky.  Why?
+  }
 
   ledcWrite(TFT_BL, tftBL_Lvl);  // Activate whatever was decided on.
   Serial.printf("%02i:%02i:%02i - Brightness level for hour %i set to %i\r\n",
@@ -290,4 +306,151 @@ void SaveOptions()
     // }
     // preferences.end();
   }
+}
+/*******************************************************************************************/
+void printVers()
+/*******************************************************************************************/
+{
+  int      lastDot, lastV;
+  String   sTemp;
+
+  //  Serial.println(__FILENAME__);  // Same as __FILE__
+  sTemp = String(__FILE__);
+  // Get rid of the trailing .ino tab name. In this case, "\Utilities.ino"
+  sTemp = sTemp.substring(0, sTemp.lastIndexOf("\\"));
+  Serial.print("Running from: "); Serial.println(sTemp);
+
+  sTemp = String(__FILE__);  // Start again for the version number.
+  lastDot = sTemp.lastIndexOf(".");
+  if (lastDot > -1) {  // Found a dot.  Thank goodness!
+    lastV = sTemp.lastIndexOf("v");  // Find start of version number
+    if (lastV > -1) {  // Oh, good, found version number, too
+      sVer = sTemp.substring(lastV + 1, lastDot); // Pick up version number
+      lastV = sVer.lastIndexOf("\\");
+      if (lastV > -1) sVer = sVer.substring(0, lastV);
+    } else {
+      sVer = "0.00";  // Unknown version.
+    }
+  } else {
+    sVer = "n/a";  // Something badly wrong here!
+  }
+  Serial.print("Version " + sVer + ", ");
+  Serial.printf("Compiled on %s at %s.\r\n", __DATE__, __TIME__);
+}
+/*******************************************************************************************/
+void AddStars()
+/*******************************************************************************************/
+{
+  static int timeForStars = 0;
+
+  // y value is optional (default is 0, so no up/down scroll).
+  spriteSF_Base.scroll(-1);  // Scroll left by one pixel
+  if (timeForStars++ == 1) {  // Was 3
+    timeForStars = 0;
+    // Redraw new stars appearing on the right
+    count = random(3);
+    //    Serial.printf("Creating %i stars.\r\n", count);
+    for (int i = 0; i < count; i++) { // Few new stars each frame
+      brightness = random(155) + 100;
+      // Serial.printf("Brightness %i ", brightness);
+      // Note:
+      // I took out the final color so it will use whatever is around the dot to be put in.
+      // That means that the alpha blend will use whatever is there instead of forcing black.
+      // If a new start goes in right next to another one, it will see some white as well as
+      //  some black.  As it should be!
+      //
+      // Also note:
+      // I made the spriteSF_Base wider than the spriteSF (SF stands for star field). Then
+      //  I make stars on the right end of spriteSF_Base but they can't be seen right away.
+      // Later, they scroll into view as if appearing in a window, as it should be.  The
+      //  same effect happens when a star scrolls out from behind the disk of the moon.  It
+      //  is occluded behind the moon then scrolls back into view on the left side of the
+      //  disk as the star field scrolls left.  As it should be!
+      spriteSF_Base.fillSmoothCircle(spriteSF_Base.width() - 4, random(spriteSF.height()),
+                                     random(3), RGB565(brightness, brightness, brightness));
+    }
+  }
+}
+void HandleSerialInput()
+/*******************************************************************************************/
+{
+  char input = Serial.read();  // Read one character from the serial input
+  int i, iHTemp;
+
+  input = toupper(input); // Convert the character to uppercase
+  if (input != '\r' && input != '\n')
+    Serial.printf("User input %c\r\n", input);  // Show user input
+
+  // In use:
+  //     D turns on the display at default brightness.  If already on, turns it off.
+  //     H (testing) to see all Hourly brightness values on the Monitor.");
+  //     ? for this list.  Upper or Lower case OK.");
+  //     default action - Show a message that the user is drunk!  ;-))
+
+  switch (input) {
+
+    case 'D':
+      if (tftBL_Lvl == 0) {
+        Serial.println("Turning screen on.");
+        tft.writecommand(ST7789_DISPON);  // Turn on display hardware.
+        tftBL_Lvl = defaultBright;
+        prevBL_Lvl = tftBL_Lvl;
+        ledcWrite(TFT_BL, tftBL_Lvl);  // Turn on the backlight
+      } else {
+        tft.fillScreen(TFT_BLACK);
+        ledcWrite(TFT_BL, defaultBright);  // Activate whatever was decided on.
+        delay(10);
+        tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+        tft.drawString("Turning off", tft.width() / 2, dispLine3, 4);
+        tft.drawString("screen.", tft.width() / 2, dispLine4, 4);
+        Serial.println("Turning screen off.");
+        delay(5000);
+        tft.fillScreen(TFT_BLACK);
+        ledcWrite(TFT_BL, 0);  // Turn off the backlight
+        tftBL_Lvl = 0;
+        prevBL_Lvl = tftBL_Lvl;
+        tft.writecommand(ST7789_DISPOFF);  // Turn off display hardware.
+      }
+      showInputOptions();
+      break;
+
+    case 'H':  // Show hourly brightness values
+      preferences.begin("Hourlys", RO_MODE);
+      for (i = 0; i < 24; i++) {
+        sprintf(chHour, "%d", i);
+        iHTemp = preferences.getInt(chHour, 1000);  // Never been set?
+        Serial.printf("Brightness level for hour %2i is %4i\r\n", i, iHTemp);
+      }
+      preferences.end();
+      showInputOptions();
+      break;
+
+    case '?':
+      showInputOptionsFull();
+      break;
+
+    case '\r':
+    case '\n':
+      break;
+
+    default:
+      Serial.printf("*>> Unknown input \'%c\'!\r\n", input);  // Handle unknown input
+      showInputOptions();
+      break;
+  }
+}
+/*******************************************************************************************/
+void showInputOptions()
+/*******************************************************************************************/
+{
+  Serial.println("Enter ? to see the full list of options.");
+}
+/*******************************************************************************************/
+void showInputOptionsFull()
+/*******************************************************************************************/
+{
+  Serial.println("Enter D to turn on the display at default brightness or off.");
+  Serial.println("Enter H (testing) to see all Hourly brightness values on the Monitor.");
+  Serial.println("Enter ? for this list.  Upper or Lower case OK.");
+  Serial.println("If you make an unknown entry, this list is printed on the monitor.\r\n");
 }
